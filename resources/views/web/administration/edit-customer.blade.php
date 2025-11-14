4ffb24a2635a45b9e9ab371e1e8d8718d08c1826
projectmk256 <projectsophisticated@gmail.com>
2025-11-14 10:54:20 +0300

    Setup

--- resources/views/web/administration/edit-customer.blade.php
+++ resources/views/web/administration/edit-customer.blade.php
@@ -94,8 +94,8 @@
                         <div class="col-md-4 form-control-validation">
                             <label class="form-label fs-5" for="customer">Customer</label>
                            <div class="input-group input-group-merge">
-                                <span class="input-group-text" id="basic-addon-user">
-                                    <i class="icon-base ti tabler-customer"></i>
+                                <span class="input-group-text" id="basic-addon-customer">
+                                    <i class="icon-base ti tabler-user"></i>
                                 </span>                                    
                                 <input type="text" value="{{ $customer->customer}}" id="customer" name="customer" class="form-control" placeholder="Enter Customer" style="text-transform:;" aria-describedby="basic-addon-customer" maxlength="11" />                        
                             </div> 
